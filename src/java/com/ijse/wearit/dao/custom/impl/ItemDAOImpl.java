512cc6222c79c8b6552ce8452c10775d587c4441
Amandi Imasha <Amandiimasha43@gmail.com>
2017-10-05 14:09:55 +0530

    implement ItemDAO contract

---
+++ src/java/com/ijse/wearit/dao/custom/impl/ItemDAOImpl.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ijse.wearit.dao.custom.impl;
+
+import com.ijse.wearit.dao.custom.ItemDAO;
+import com.ijse.wearit.model.Item;
+import java.util.List;
+
+/**
+ *
+ * @author ABC
+ */
+public class ItemDAOImpl implements ItemDAO{
+
+    @Override
+    public boolean add(Item t) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean update(Item t) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean delete(Integer id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Item search(Integer id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Item> getAll() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
