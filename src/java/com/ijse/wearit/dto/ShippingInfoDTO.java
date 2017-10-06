069bdcac181437e924c648b2255b068ed7ca2ff4
Amandi Imasha <Amandiimasha43@gmail.com>
2017-10-06 23:58:15 +0530

    create ShippingInfoDTO

---
+++ src/java/com/ijse/wearit/dto/ShippingInfoDTO.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ijse.wearit.dto;
+
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author ABC
+ */
+@Component
+public class ShippingInfoDTO {
+    private Integer id;
+    private String FName;
+    private String LName;
+    private String AddressLine1;
+    private String City;
+    private String contry;
+    private String PostalCode;
+    private String ContactNum;
+    private UserDTO userDTO;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFName() {
+        return FName;
+    }
+
+    public void setFName(String FName) {
+        this.FName = FName;
+    }
+
+    public String getLName() {
+        return LName;
+    }
+
+    public void setLName(String LName) {
+        this.LName = LName;
+    }
+
+    public String getAddressLine1() {
+        return AddressLine1;
+    }
+
+    public void setAddressLine1(String AddressLine1) {
+        this.AddressLine1 = AddressLine1;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getContry() {
+        return contry;
+    }
+
+    public void setContry(String contry) {
+        this.contry = contry;
+    }
+
+    public String getPostalCode() {
+        return PostalCode;
+    }
+
+    public void setPostalCode(String PostalCode) {
+        this.PostalCode = PostalCode;
+    }
+
+    public String getContactNum() {
+        return ContactNum;
+    }
+
+    public void setContactNum(String ContactNum) {
+        this.ContactNum = ContactNum;
+    }
+
+    public UserDTO getUserDTO() {
+        return userDTO;
+    }
+
+    public void setUserDTO(UserDTO userDTO) {
+        this.userDTO = userDTO;
+    }
+    
+    
+}
