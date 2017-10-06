24d5cd07ef226f205f67508ceacf9f8214ac7257
Amandi Imasha <Amandiimasha43@gmail.com>
2017-10-07 03:07:04 +0530

    create ItemDetailsIdDTO

---
+++ src/java/com/ijse/wearit/dto/ItemDetailsIdDTO.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ijse.wearit.dto;
+
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author ABC
+ */
+@Component
+public class ItemDetailsIdDTO extends SuperDTO{
+    private SizeDTO sizeDTO;
+    private ItemDTO itemDTO;
+
+    public SizeDTO getSizeDTO() {
+        return sizeDTO;
+    }
+
+    public void setSizeDTO(SizeDTO sizeDTO) {
+        this.sizeDTO = sizeDTO;
+    }
+
+    public ItemDTO getItemDTO() {
+        return itemDTO;
+    }
+
+    public void setItemDTO(ItemDTO itemDTO) {
+        this.itemDTO = itemDTO;
+    }
+    
+    
+    
+}
