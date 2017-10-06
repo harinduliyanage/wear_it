f81a82ed7227e48fe8f97b28e365a951ca6c03ab
Amandi Imasha <Amandiimasha43@gmail.com>
2017-10-06 12:26:01 +0530

    implement ShoppingCartDetailsDAO contract

---
+++ src/java/com/ijse/wearit/dao/custom/ShoppingCartDetailsDAO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ijse.wearit.dao.custom;
+
+import com.ijse.wearit.dao.SuperDAO;
+import com.ijse.wearit.model.ShoppingCartDetails;
+
+/**
+ *
+ * @author ABC
+ */
+public interface ShoppingCartDetailsDAO extends SuperDAO<ShoppingCartDetails>{
+    
+}
