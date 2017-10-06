c132ec8eae64fc662ec80402e50a228b9988200c
harinduliyanage <harindu.sul@gmail.com>
2017-10-06 03:08:35 -0700

    create DTO

---
+++ src/java/com/ijse/wearit/dto/CategoryDTO.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ijse.wearit.dto;
+
+import com.ijse.wearit.model.Item;
+import java.util.Set;
+
+/**
+ *
+ * @author Harindu.sul
+ */
+public class CategoryDTO {
+    private Integer id;
+    private String name;
+    private Set<Item> items;
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the items
+     */
+    public Set<Item> getItems() {
+        return items;
+    }
+
+    /**
+     * @param items the items to set
+     */
+    public void setItems(Set<Item> items) {
+        this.items = items;
+    }
+    
+}
