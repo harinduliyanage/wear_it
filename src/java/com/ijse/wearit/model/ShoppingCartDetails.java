10778278c2ac8ec034ad7a34c88cc9dc95c7d6c9
harinduliyanage <harindu.sul@gmail.com>
2017-10-06 21:04:38 -0700

    many to many

--- src/java/com/ijse/wearit/model/ShoppingCartDetails.java
+++ src/java/com/ijse/wearit/model/ShoppingCartDetails.java
@@ -7,9 +7,11 @@ package com.ijse.wearit.model;
 
 import javax.persistence.AssociationOverride;
 import javax.persistence.AssociationOverrides;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 /**
  *
@@ -18,10 +20,52 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "SHOPPINGCART_DETAILS")
 @AssociationOverrides({
-    @AssociationOverride(name = "primaryKey.ShoppingCart",
-        joinColumns = @JoinColumn(name = "SHOPPINGCART_ID")),
-    @AssociationOverride(name = "primaryKey.ITEM_DETAILS",
+    @AssociationOverride(name = "primaryKey.shoppingCart",
+        joinColumns = @JoinColumn(name = "CART_ID")),
+    @AssociationOverride(name = "primaryKey.itemDetails",
         joinColumns = @JoinColumn(name = "ITEMDETAILS_ID")) })
 public class ShoppingCartDetails {
+    private ShoppingCartDetailCompositeId primaryKey = new ShoppingCartDetailCompositeId();
+    
+    @EmbeddedId
+    public ShoppingCartDetailCompositeId getPrimaryKey() {
+        return primaryKey;
+    }
+ 
+    public void setPrimaryKey(ShoppingCartDetailCompositeId primaryKey) {
+        this.primaryKey = primaryKey;
+    }
+    @Transient
+    public ShoppingCart getShoppingCart() {
+        return getPrimaryKey().getShoppingCart();
+    }
+ 
+    public void setShoppingCart(ShoppingCart shoppingCart) {
+        getPrimaryKey().setShoppingCart(shoppingCart);
+    }
+ 
+    @Transient
+    public ItemDetails getItemDetails() {
+        return getPrimaryKey().getItemDetails();
+    }
+ 
+    public void setItemDetails(ItemDetails itemDetails) {
+        getPrimaryKey().setItemDetails(itemDetails);
+    }
+    public String getDescription() {
+        return getPrimaryKey().getItemDetails().getItem().getDescription();
+    }
+ 
+    public void setDescription(String description) {
+        getPrimaryKey().getItemDetails().getItem().setDescription(description);
+    }
+    
+    public String getPath() {
+        return getPrimaryKey().getItemDetails().getItem().getPath();
+    }
+ 
+    public void setPath(String path) {
+        getPrimaryKey().getItemDetails().getItem().setPath(path);
+    }
     
 }
