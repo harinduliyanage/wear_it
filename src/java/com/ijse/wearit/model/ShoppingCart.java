8944af478875c200771507d983b5b57e8cdb940c
harinduliyanage <harindu.sul@gmail.com>
2017-10-06 19:39:37 -0700

    mapped itemDetails to shopping cart

--- src/java/com/ijse/wearit/model/ShoppingCart.java
+++ src/java/com/ijse/wearit/model/ShoppingCart.java
@@ -6,11 +6,15 @@
 package com.ijse.wearit.model;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 /**
@@ -19,17 +23,18 @@ import javax.persistence.OneToOne;
  */
 @Entity
 public class ShoppingCart implements Serializable{
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String addedDate;
-    
-    @OneToOne(cascade = CascadeType.ALL)
     private User user;
+    
+    private Set<ShoppingCartDetails> shoppingCartDetails = new HashSet<ShoppingCartDetails>();
 
     public ShoppingCart() {
     }
-
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "USER_ID")
     public Integer getShoppingcartID() {
         return id;
     }
@@ -46,6 +51,7 @@ public class ShoppingCart implements Serializable{
         this.addedDate = addedDate;
     }
 
+    @OneToOne(cascade = CascadeType.ALL)
     public User getUser() {
         return user;
     }
@@ -53,5 +59,19 @@ public class ShoppingCart implements Serializable{
     public void setUser(User user) {
         this.user = user;
     }
+    
+    @OneToMany(mappedBy = "primaryKey.shoppingcart",
+            cascade = CascadeType.ALL)
+    public Set<ShoppingCartDetails> getShoppingCartDetails() {
+        return shoppingCartDetails;
+    }
+ 
+    public void setUserGroups(Set<ShoppingCartDetails> shoppingCartDetailses) {
+        this.shoppingCartDetails = shoppingCartDetailses;
+    }
+     
+    public void addUserGroup(ShoppingCartDetails shoppingCartDetail) {
+        this.shoppingCartDetails.add(shoppingCartDetail);
+    }  
     
 }
