c300a25952bb48098ed13917f6e9b4a529ad5749
Amandi Imasha <Amandiimasha43@gmail.com>
2017-10-07 23:32:30 +0530

    ShippingInfoService interface created

---
+++ src/java/com/ijse/wearit/service/custom/ShippingInfoService.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ijse.wearit.service.custom;
+
+import com.ijse.wearit.dao.SuperDAO;
+import com.ijse.wearit.model.ShippingInfo;
+import com.ijse.wearit.service.SuperService;
+
+/**
+ *
+ * @author ABC
+ */
+public interface ShippingInfoService extends SuperService<ShippingInfo>{
+    
+}
