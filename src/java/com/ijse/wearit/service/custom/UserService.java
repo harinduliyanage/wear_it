0449eaba373f08508f5c9f96f07261097f61a408
Amandi Imasha <Amandiimasha43@gmail.com>
2017-10-08 00:07:59 +0530

    UserService interface created

---
+++ src/java/com/ijse/wearit/service/custom/UserService.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ijse.wearit.service.custom;
+
+import com.ijse.wearit.model.User;
+import com.ijse.wearit.service.SuperService;
+
+/**
+ *
+ * @author ABC
+ */
+public interface UserService extends SuperService<UserService>{
+    
+}
