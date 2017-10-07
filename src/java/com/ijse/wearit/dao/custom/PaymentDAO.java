8c96d2abc0b54732b085ff746adb198b6aaa00c8
harinduliyanage <harindu.sul@gmail.com>
2017-10-07 02:19:06 -0700

    create Payment DAO inteface

---
+++ src/java/com/ijse/wearit/dao/custom/PaymentDAO.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ijse.wearit.dao.custom;
+
+import com.ijse.wearit.dao.SuperDAO;
+import com.ijse.wearit.model.Payment;
+
+
+/**
+ *
+ * @author Harindu.sul
+ */
+public interface PaymentDAO extends SuperDAO<Payment>{
+    
+}
