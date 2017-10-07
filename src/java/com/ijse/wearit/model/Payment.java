0880f3bba9aa39caab1b4b7b247c14e7874c040e
harinduliyanage <harindu.sul@gmail.com>
2017-10-07 00:18:37 -0700

    Payment Model Created

---
+++ src/java/com/ijse/wearit/model/Payment.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ijse.wearit.model;
+
+import java.io.Serializable;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author Harindu.sul
+ */
+@Entity
+class Payment  implements Serializable{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)        
+    private Integer id;
+    @ManyToOne(cascade = CascadeType.ALL)
+    private PaymentMethod paymentMethod;
+    private String PaymentResponseMessage;
+    private String userIdOrSessionId;
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the paymentMethod
+     */
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    /**
+     * @param paymentMethod the paymentMethod to set
+     */
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    /**
+     * @return the PaymentResponseMessage
+     */
+    public String getPaymentResponseMessage() {
+        return PaymentResponseMessage;
+    }
+
+    /**
+     * @param PaymentResponseMessage the PaymentResponseMessage to set
+     */
+    public void setPaymentResponseMessage(String PaymentResponseMessage) {
+        this.PaymentResponseMessage = PaymentResponseMessage;
+    }
+
+    /**
+     * @return the userIdOrSessionId
+     */
+    public String getUserIdOrSessionId() {
+        return userIdOrSessionId;
+    }
+
+    /**
+     * @param userIdOrSessionId the userIdOrSessionId to set
+     */
+    public void setUserIdOrSessionId(String userIdOrSessionId) {
+        this.userIdOrSessionId = userIdOrSessionId;
+    }
+
+   
+    
+}
