21ba84215cd5bb34a7dd10018cd1354a664b5edc
harinduliyanage <harindu.sul@gmail.com>
2017-10-07 19:41:54 -0700

    sizeServiceImpl created

---
+++ src/java/com/ijse/wearit/service/custom/impl/SizeServiceImpl.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ijse.wearit.service.custom.impl;
+
+import com.ijse.wearit.dao.custom.SizeDAO;
+import com.ijse.wearit.model.Size;
+import com.ijse.wearit.service.custom.SizeService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author Harindu.sul
+ */
+@Service
+@Transactional
+public class SizeServiceImpl implements SizeService{
+    
+    @Autowired
+    SizeDAO sizeDAOImpl;
+
+    @Override
+    public boolean add(Size t) throws Exception {
+        return sizeDAOImpl.add(t);
+    }
+
+    @Override
+    public boolean update(Size t) throws Exception {
+        return sizeDAOImpl.update(t);
+    }
+
+    @Override
+    public boolean delete(Integer id) throws Exception {
+        return sizeDAOImpl.delete(id);
+    }
+
+    @Override
+    public Size search(Integer id) throws Exception {
+        return sizeDAOImpl.search(id);
+    }
+
+    @Override
+    public List<Size> getAll() throws Exception {
+        return sizeDAOImpl.getAll();
+    }
+    
+}
