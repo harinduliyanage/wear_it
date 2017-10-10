62f577969b9c15b031abad2132b5646793c43fb2
harinduliyanage <harindu.sul@gmail.com>
2017-10-10 01:47:20 -0700

    Sizes model created

---
+++ src/java/com/ijse/wearit/model/Sizes.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ijse.wearit.model;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author Harindu.sul
+ */
+@Entity
+public class Sizes implements Serializable{ 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String sizes;//Xs,S,M,L,XL
+    private String sizeUK;
+    private String sizeUS;
+    private String sizeEU;
+    
+    @OneToMany(mappedBy = "sizes",fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+    Set<ItemDetails> itemDetails=new HashSet<ItemDetails>();
+
+    public Sizes() {
+    }
+    
+    public void setItemDetails(Set<ItemDetails> itemDetails){
+        this.itemDetails=itemDetails;
+    }
+    
+    public Set<ItemDetails> getItemDetails(){
+        return this.itemDetails;
+    }
+
+    public String getSizeUK() {
+        return sizeUK;
+    }
+
+    public void setSizeUK(String sizeUK) {
+        this.sizeUK = sizeUK;
+    }
+
+    public String getSizeUS() {
+        return sizeUS;
+    }
+
+    public void setSizeUS(String sizeUS) {
+        this.sizeUS = sizeUS;
+    }
+
+    public String getSizeEU() {
+        return sizeEU;
+    }
+
+    public void setSizeEU(String sizeEU) {
+        this.sizeEU = sizeEU;
+    }
+
+    
+    public void setSizes(String sizes) {
+        this.sizes = sizes;
+    }
+
+    public String getSizes() {
+        return sizes;
+    }
+
+    
+    public Integer getId() {
+        return id;
+    }
+
+    
+    public void setId(Integer id) {
+        this.id = id;
+    }
+  
+}
