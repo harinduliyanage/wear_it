1ae4f025c2990670bed8bb8770326f50b85fe567
harinduliyanage <harindu.sul@gmail.com>
2017-10-16 09:36:08 -0700

    add dom-register js to handle register page functions

---
+++ web/resources/js/dom-register.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$('#signUp-1st-btn').click(function (){
+    var name=$('#name-txt').val();
+    var phone=$('#phone-txt').val();
+    var email=$('#email-txt').val();
+    var pw=$('#psw-txt').val();
+    console.log(name+"----"+phone+"---"+email+"--"+pw);
+//    if (name!==null && phone!==null && email!==null && pw!==null){
+//        $('#signUp-1st').css('display','none'); 
+//    }    
+});
