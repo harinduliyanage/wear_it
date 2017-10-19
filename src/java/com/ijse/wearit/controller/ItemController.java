448addeaa0ae5f14156e3ee72352edb9f9dbae2f
Amandi Imasha <Amandiimasha43@gmail.com>
2017-10-19 17:46:48 +0530

    Updated...

--- src/java/com/ijse/wearit/controller/ItemController.java
+++ src/java/com/ijse/wearit/controller/ItemController.java
@@ -7,7 +7,9 @@ package com.ijse.wearit.controller;
 
 import com.ijse.wearit.dto.ItemDTO;
 import com.ijse.wearit.model.Item;
+import com.ijse.wearit.model.ItemDetails;
 import com.ijse.wearit.model.Status;
+import com.ijse.wearit.service.custom.ItemDetailsService;
 import com.ijse.wearit.service.custom.ItemService;
 import java.util.List;
 import java.util.logging.Level;
@@ -30,6 +32,9 @@ public class ItemController {
     
     @Autowired
     ItemService itemService;
+    
+    @Autowired
+    ItemDetailsService itemDetailsService;
     
     @RequestMapping(value = "/getAllItems" , method = RequestMethod.GET)
     public @ResponseBody List<Item>  getAllItemss(){ 
@@ -70,39 +75,54 @@ public class ItemController {
             }
 	}
     
-//        @RequestMapping(value = "/deleteItem", method = RequestMethod.POST)
-//        public @ResponseBody Status deleteItem(@RequestParam("description") String description){
-//            Status status=null;
-//            String staticPath="C:\\Users\\Harindu.sul\\Documents\\Project\\wear_it\\build\\web\\";
-//            try{
-//                //you should use transacton delete 
-//                //code here to search item by description
-//                //then get Item have any item details and delete itemDetails
-//                //then searchitem.getPath();
-//                
-//                String givenDeletedPath=staticPath+searchitem.getPath;//database eke thiyena path eka mekata concat karanna + operator eka use karanna me widihata
-//    		File file = new File(givenDeletedPath);
-//                if(file.delete()){
-//                   // code here delete item object
-//                   //System.out.println(file.getName() + " is deleted!");
-//                   //return OK status
-//                }else{
-//                    System.out.println("Delete operation is failed.");
-//                }
-//
-//
-//            }catch(Exception e){
-//                System.out.println(e);
-//
-//            }
-//            return status;
-//        }
+        @RequestMapping(value = "/deleteItem", method = RequestMethod.POST)
+        public @ResponseBody Status deleteItem(@RequestParam("description") String description){
+            Status status = new Status();
+            boolean result = false;
+
+            try {
+                result=itemService.deleteItemByDescription(description);
+                if(result){
+                    status  = new Status(200, "Ok", "Added Successfully...");
+                    return status;
+                }else{
+                    status = new Status(500, "Internal Server Error", "Added Faild..");
+                    return status;
+                }
+            } catch (Exception ex) {
+                Logger.getLogger(ItemController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return status;
+        }
+    
+        @RequestMapping(value = "/getItemByDescription", method = RequestMethod.POST)
+        public @ResponseBody Item getItemByDescription(
+                @RequestParam("description") String description){
         
+            Item item = null;
+        try {
+            item = itemService.getItemByDescription(description);
+        } catch (Exception ex) {
+            Logger.getLogger(ItemController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            return item;
+        
+        }
     
-        //create method getItemByDescription 
-    
-    
-        //create method getItemDetailsList by item description
+        @RequestMapping(value = "/getItemDetailsList", method = RequestMethod.GET)
+        public @ResponseBody List<ItemDetails> getItemDetailsList(
+                @RequestParam("description") String description){
+            
+            List<ItemDetails> itemDetailsList = null;
+            try {
+                Item searchedItem = itemService.getItemByDescription(description);
+                itemDetailsList = itemDetailsService.searchByItemID(searchedItem.getItemCode());
+            } catch (Exception ex) {
+                Logger.getLogger(ItemController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return itemDetailsList;
+        }
+        
         
     
 }
